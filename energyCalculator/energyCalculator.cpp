8c204fb5c2ba18b6ba1c4539d1f6980172702e8f
bamuelsarfi <barfisamuel29@gmail.com>
2024-05-16 20:47:45 +0100

    fixing bugs and adding 'choice'

--- energyCalculator/energyCalculator.cpp
+++ energyCalculator/energyCalculator.cpp
@@ -1,3 +1,4 @@
+//written by @bamuelsarfi
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
@@ -5,7 +6,7 @@
 using namespace std;
 
 double kineticEnergy(double mass, double velocity) {
-
+	
 	return 0.5 * mass * pow(velocity, 2);
 
 }
@@ -21,15 +22,9 @@ double elasticPotential(double springConst, double ext) {
 
 }
 
-//kinetic energy: 0.5 * m * v^2
-//gpe = m * g * h
-//epe = 0.5 * k * e^2
-
-int main()
+int begin(bool isFinish)
 {
 	int num;
-	bool isFinish = false;
-
 	cout << "**WELCOME TO ENERGY CALCULATOR.**\n";
 	cout << "enter '1' for kinetic energy, '2' for gravitational potential energy, and '3' for elastic potential energy.\n";
 	cout << "Enter here >>> ";
@@ -37,7 +32,7 @@ int main()
 
 	if (num == 1) {
 		double mass, velocity;
-		bool isFinish;
+
 
 		cout << "enter mass(g): ";
 		cin >> mass;
@@ -47,12 +42,12 @@ int main()
 
 		kineticEnergy(mass, velocity);
 
-		cout << "Result : " << kineticEnergy(mass, velocity) << endl;
+		cout << "\nResult : " << kineticEnergy(mass, velocity) << endl;
 
 
 		if (nearbyint(kineticEnergy(mass, velocity)) != kineticEnergy(mass, velocity)) {
 
-			cout << "Rounded result: " << nearbyint(kineticEnergy(mass, velocity));
+			cout << "Rounded result: " << nearbyint(kineticEnergy(mass, velocity)) << endl;
 			isFinish = true;
 
 		}
@@ -64,25 +59,26 @@ int main()
 
 	}
 	else if (num == 2) {
-		double mass, gravField, height;
-		bool isFinish;
+		double mass, gravField, height = NULL;
 
-		cout << "enter mass: ";
+
+		cout << "enter mass(g): ";
 		cin >> mass;
 
-		cout << "enter gravitational field: ";
+		cout << "enter gravitational field(N/kg): ";
 		cin >> gravField;
 
-		cout << "enter height: ";
+
+		cout << "enter height(cm): ";
 		cin >> height;
 
 		gravitationalPotential(mass, gravField, height);
 
-		cout << "Result :" << gravitationalPotential(mass, gravField, height) << endl;
+		cout << "\nResult :" << gravitationalPotential(mass, gravField, height) << endl;
 
 		if (gravitationalPotential(mass, gravField, height) != nearbyint(gravitationalPotential(mass, gravField, height))) {
 
-			cout << "Rounded result: " << nearbyint(gravitationalPotential(mass, gravField, height));
+			cout << "Rounded result: " << nearbyint(gravitationalPotential(mass, gravField, height)) << endl;
 			isFinish = true;
 
 
@@ -97,23 +93,23 @@ int main()
 	else if (num == 3) {
 		double springConst;
 		double ext;
-		bool isFinish;
 
-		cout << "enter spring constant: ";
+
+		cout << "enter spring constant(N/m): ";
 		cin >> springConst;
 
-		cout << "enter spring extention: ";
+		cout << "enter spring extention(m): ";
 		cin >> ext;
 
 
 		elasticPotential(springConst, ext);
 
-		cout << "Result: " << elasticPotential(springConst, ext) << endl;
+		cout << "\nResult: " << elasticPotential(springConst, ext) << endl;
 
 
 		if (elasticPotential(springConst, ext) != nearbyint(elasticPotential(springConst, ext))) {
 
-			cout << "Rounded Result: " << nearbyint(elasticPotential(springConst, ext));
+			cout << "Rounded Result: " << nearbyint(elasticPotential(springConst, ext)) << endl;
 			isFinish = true;
 
 		}
@@ -126,14 +122,43 @@ int main()
 
 
 	}
-
-
-	if (isFinish == true) {
-
-		cout << "tick";
-		isFinish = false;
+	else {
+		cout << "Enter a number between 1 and 3";//update everytime you add a calculation. 
 		return 8;
+	}
+}
 
+//kinetic energy: 0.5 * m * v^2
+//gpe = m * g * h
+//epe = 0.5 * k * e^2
+//gravitational feild strength for the earth is 9.8
+
+int main()
+{
+	bool isFinish = false;
+	
+	while (isFinish != true) {
+		begin(isFinish);
+	}
+	if (isFinish == true) {
+		char choice = NULL;
+		isFinish = false;
+		cout << "would you like to do it again(y/n): ";
+		cin >> choice;
+
+		if (choice == 'y' || choice == 'Y') {
+			begin(isFinish);
+		}
+		else if (choice == 'n' || choice == 'N') {
+			cout << "Thanks for using ENERGY CALCULATOR.";
+			return 7;
+		}
+		else {
+			cout << "\nEnter either 'y' or 'n': ";
+			cin >> choice;
+		}
+
+		
 	}
 
 	return 0;
